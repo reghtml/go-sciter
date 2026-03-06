89b48c5ef43aefbdd7e78290a42b746e2887111f
reghtml <979702097@qq.com>
2026-03-06 13:54:11 +0800

    保存demo中的库名称

--- window/window.go
+++ window/window.go
@@ -1,22 +1,22 @@
 package window
 
 import (
-    "runtime"
+	"runtime"
 
-    "github.com/zhaobingss/go-sciter"
+	"github.com/reghtml/go-sciter"
 )
 
 type Window struct {
-    *sciter.Sciter
-    creationFlags sciter.WindowCreationFlag
+	*sciter.Sciter
+	creationFlags sciter.WindowCreationFlag
 }
 
 func (w *Window) run() {
-    // runtime.LockOSThread()
+	// runtime.LockOSThread()
 }
 
 // https://github.com/golang/go/wiki/LockOSThread
-// https://github.com/zhaobingss/go-sciter/issues/201
+// https://github.com/reghtml/go-sciter/issues/201
 func init() {
-    runtime.LockOSThread()
+	runtime.LockOSThread()
 }
